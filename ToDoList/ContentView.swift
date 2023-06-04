ab4717d2238cd2c7ee97c5e8c1be74ebfaf760b1
Kayley Chery <kac7748@nyu.edu>
2023-06-03 23:00:15 -0400

    finished ToDoList Project!

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -9,14 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var showNewTask = false
-    @State var toDoItems: [ToDoItem] = []
+    @FetchRequest(
+            entity: ToDo.entity(), sortDescriptors: [ NSSortDescriptor(keyPath: \ToDo.id, ascending: false) ])
+        
+    var toDoItems: FetchedResults<ToDo>
     
     var body: some View {
         VStack {
             HStack {
-                Text("To Do List")
+                Text("The \nUltimate \nTo Do List")
                 .font(.system(size: 40))
                 .fontWeight(.black)
+                .foregroundColor(Color.purple)
                 
             Spacer()
              
@@ -24,6 +28,9 @@ struct ContentView: View {
                 self.showNewTask = true
             }) {
                 Text("+")
+                    .font(.system(size: 100))
+                    .fontWeight(.black)
+                    .foregroundColor(Color.purple)
                 }
             
             }
@@ -35,11 +42,11 @@ struct ContentView: View {
             
             ForEach (toDoItems) { toDoItem in
                 if toDoItem.isImportant == true {
-                    Text("‼️" + toDoItem.title)
+                    Text("‼️" + (toDoItem.title ?? "No title"))
+                } else {
+                                    Text(toDoItem.title ?? "No title")
                 }
-                else {
-                    Text(toDoItem.title)
-                }
+
                             
                
             }
@@ -47,11 +54,7 @@ struct ContentView: View {
         .listStyle(.plain)
         
         if showNewTask {
-            NewToDoView(title: "",
-                        isImportant: false,
-                        toDoItems: $toDoItems ,
-                        showNewTask: $showNewTask
-                        )
+            NewToDoView(title: "", isImportant: false, showNewTask: $showNewTask)
             }
 
     }
--- ToDoList/NewToDoView.swift
+++ ToDoList/NewToDoView.swift
@@ -8,52 +8,70 @@
 import SwiftUI
 
 struct NewToDoView: View {
+    @Environment(\.managedObjectContext) var context
     @State var title: String
     @State var isImportant: Bool
-    @Binding var toDoItems: [ToDoItem]
     @Binding var showNewTask : Bool
-    
+    let peach = Color("Color")
     var body: some View {
-        
-        VStack {
-            Text("Add a new task")
-                .font(.title)
-                .fontWeight(.bold)
-            
-            TextField("Enter the task description", text: $title)
+       
+            VStack {
+              
+                    
+                Text("✨Add a new task✨")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.purple)
+                
+                
+                TextField("Enter the task description", text: $title)
+                    .padding()
+                    .foregroundColor(Color.purple)
+                    .background(Color(.systemGroupedBackground))
+                    .cornerRadius(15)
+                    .padding()
+                
+                Toggle(isOn: $isImportant) {
+                    Text("Is it important?")
+                        .foregroundColor(Color.purple)
+                }
+                .tint(.purple)
                 .padding()
-                .background(Color(.systemGroupedBackground))
-                .cornerRadius(15)
-                .padding()
-            
-            Toggle(isOn: $isImportant) {
-                Text("Is it important?")
+                
+                Button(action: {
+                    self.addTask(title: self.title, isImportant: self.isImportant)
+                    self.showNewTask = false
+                }) {
+                    Text("Add")
+                        .foregroundColor(Color.purple)
+                        .buttonStyle(.borderedProminent)
+                }
             }
-            .padding()
             
-            Button(action: {
-                self.addTask(title: self.title, isImportant: self.isImportant)
-                self.showNewTask = false
-            }) {
-                Text("Add")
+            
+        }
+        private func addTask(title: String, isImportant: Bool = false) {
+            
+            let task = ToDo(context: context)
+            task.id = UUID()
+            task.title = title
+            task.isImportant = isImportant
+            
+            do {
+                try context.save()
+            } catch {
+                print(error)
             }
         }
-        
-        
     }
-    private func addTask(title: String, isImportant: Bool = false) {
-            
-            let task = ToDoItem(title: title, isImportant: isImportant)
-            toDoItems.append(task)
-    }
-}
+
 
 
 
 
 struct NewToDoView_Previews: PreviewProvider {
     static var previews: some View {
-        NewToDoView(title: "", isImportant: false,toDoItems: .constant([]), showNewTask: .constant(true))
+        NewToDoView(title: "", isImportant: false, showNewTask: .constant(true))
     }
    
 }
